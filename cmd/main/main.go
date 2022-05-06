dacd419862302ec8b200f756bc440fbccd7a33e7
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-05-06 16:43:11 +0300

    update doc

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,11 @@
+/* Main application package with main function (entrypoint) */
 package main
 
 import (
 	"webimizer.dev/poem"
 )
 
+/* Application entrypoint */
 func main() {
 	poem.InitApplication()
 }
