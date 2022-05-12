1a96c7edbcd7a16a915c9c9674dc979d8a16bf74
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-05-12 15:52:33 +0300

    add admin-server subcommand test

--- poem_test.go
+++ poem_test.go
@@ -32,12 +32,29 @@ func TestInitApplication(t *testing.T) {
 	assert.Contains(t, buf.String(), "commands")
 	assert.Contains(t, buf.String(), "flags")
 	assert.Contains(t, buf.String(), "help")
+	assert.Contains(t, buf.String(), "install")
 	assert.Contains(t, buf.String(), "poems-server")
+	assert.Contains(t, buf.String(), "admin-server")
 }
 
 /* Testing Poems server output */
 func TestInitPoemsServer(t *testing.T) {
 	os.Args = []string{"poem", "poems-server"}
+	r, w, err := os.Pipe()
+	require.NoError(t, err)
+	os.Stdout = w
+	subcommands.DefaultCommander.Output = w
+	poem.InitApplication()
+	_ = w.Close()
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, r)
+	require.NoError(t, err)
+	assert.Contains(t, buf.String(), "Starting server...")
+}
+
+/* Testing Admin server output */
+func TestInitAdminServer(t *testing.T) {
+	os.Args = []string{"poem", "admin-server"}
 	r, w, err := os.Pipe()
 	require.NoError(t, err)
 	os.Stdout = w
